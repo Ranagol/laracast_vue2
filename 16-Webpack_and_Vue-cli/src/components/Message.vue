35e4833ba608d0380238d939c4af2353ff9b200a
Andor <andorhorvat@gmail.com>
2020-01-03 16:35:23 +0100

    Lesson 16

---
+++ 16-Webpack_and_Vue-cli/src/components/Message.vue
@@ -0,0 +1,24 @@
+<template>
+
+  <div class="box">
+    <p><slot></slot></p>
+  </div>
+
+
+</template>
+
+
+<script>
+
+  export default {
+
+  }
+
+</script>
+
+
+<style>
+
+ .box { background: grey; padding: 10px; border: 1px solid blue;}
+
+</style>
